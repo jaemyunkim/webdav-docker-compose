f045421666cd24ecd16c509a86822aa5902dc406
Jaemyun Kim <jaemyunkim@outlook.com>
2024-02-13 19:29:53 +0900

    change to support blank in USERNAME and PASSWORD

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]
 then
-	htpasswd -bc /etc/nginx/htpasswd $USERNAME $PASSWORD
+	htpasswd -bc /etc/nginx/htpasswd "$USERNAME" "$PASSWORD"
 	echo Done.
 else
     echo Using no auth.
